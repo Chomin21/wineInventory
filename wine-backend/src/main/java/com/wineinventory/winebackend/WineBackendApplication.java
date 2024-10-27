9a98f3a3f1c00d451e720fc04f84015826fc4a8d
Chomin21 <cmkbooty21@naver.com>
2024-10-27 17:11:18 +0900

    프로젝트 폴더 세팅

---
+++ wine-backend/src/main/java/com/wineinventory/winebackend/WineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wineinventory.winebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineBackendApplication.class, args);
+	}
+
+}
---
+++ wine-backend/src/test/java/com/wineinventory/winebackend/WineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wineinventory.winebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
